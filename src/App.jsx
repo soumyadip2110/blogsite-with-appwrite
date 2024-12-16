d5de1ab2d8ab7117bc6ebdf4b44f7dd095b04a56
soumyadip2110 <soumyadip2110@gmail.com>
2024-12-16 17:43:40 +0530

    build auth service of appwrite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import conf from './conf/conf';
+
+function App() {
+	console.log('Project ID: ', conf.appwriteProjectId);
+	
+	return (
+		<h1>a blog app with appwrite</h1>
+	)
+}
+
+export default App
